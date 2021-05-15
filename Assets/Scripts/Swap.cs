e309acf85cb964d3e4022dbd2b343e17c553288f
Roman <carapacik@gmail.com>
2021-05-15 18:22:42 +0300

    added main scripts for level

--- Assets/Scripts/Swap.cs
+++ Assets/Scripts/Swap.cs
@@ -4,30 +4,36 @@ using UnityEngine;
 
 public class Swap : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
-    public Sprite x_belt;
-    public Sprite p_belt;
+    public GameObject _gameObject;
+    public SpriteRenderer _spriteRenderer;
+    public Sprite _x_belt;
+    public Sprite _p_belt;
+    public bool _isCorrect;
 
+    void Start()    
+    {
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+    
+    public void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+            
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null && hit.transform.gameObject == _gameObject)
+            {
+                _isCorrect ^= true;
+                if (_spriteRenderer.sprite == _x_belt)
+                    ChangeSprite(_p_belt);
+                else
+                    ChangeSprite(_x_belt);
+            }
+        }
+    }
+    
     void ChangeSprite(Sprite sprite)
     {
-        spriteRenderer.sprite = sprite; 
-    }
-
-    void Start()
-    {
-        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-    }
-
-    void Update()
-    {
-        if(Input.GetMouseButtonDown(0))
-        {
-            if (spriteRenderer.sprite == x_belt)
-            {
-                ChangeSprite(p_belt);
-            }
-            else
-                ChangeSprite(x_belt);
-        }
+        _spriteRenderer.sprite = sprite; 
     }
 }
