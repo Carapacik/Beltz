455485b625e0bc8269364c87271431e698877bbb
Roman <carapacik@gmail.com>
2021-05-16 20:46:37 +0300

    next level button

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            var hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null && hit.transform.gameObject == gameObject) SceneManager.LoadScene("Level2");
+        }
+    }
+}
