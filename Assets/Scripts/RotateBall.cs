091b1c4c95cb8c4907d7b507f973bf5707e42ba0
Roman <carapacik@gmail.com>
2021-05-16 17:18:20 +0300

    rotate balls after victory

---
+++ Assets/Scripts/RotateBall.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RotateBall : MonoBehaviour
+{
+    [SerializeField] private float rotation;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        transform.Rotate(0, 0, rotation);
+    }
+}
