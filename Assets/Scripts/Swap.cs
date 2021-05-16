37e45059aeabe400fcc6f27ed6e22c773c2d0bf1
Roman <carapacik@gmail.com>
2021-05-16 17:18:20 +0300

    rotate balls after victory

--- Assets/Scripts/Swap.cs
+++ Assets/Scripts/Swap.cs
@@ -1,39 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Swap : MonoBehaviour
 {
-    public GameObject _gameObject;
-    public SpriteRenderer _spriteRenderer;
-    public Sprite _x_belt;
-    public Sprite _p_belt;
-    public bool _isCorrect;
+    [SerializeField] private GameObject belt;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite xBelt;
+    [SerializeField] private Sprite pBelt;
+    [SerializeField] public bool isCorrect;
 
-    void Start()    
+    private void Start()
     {
-        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
-    
-    public void Update () {
-        if (Input.GetMouseButtonDown(0)) {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
-            
-            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
-            if (hit.collider != null && hit.transform.gameObject == _gameObject)
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var mousePos2D = new Vector2(mousePos.x, mousePos.y);
+
+            var hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+            if (hit.collider != null && hit.transform.gameObject == belt)
             {
-                _isCorrect ^= true;
-                if (_spriteRenderer.sprite == _x_belt)
-                    ChangeSprite(_p_belt);
-                else
-                    ChangeSprite(_x_belt);
+                isCorrect ^= true;
+                ChangeSprite(spriteRenderer.sprite == xBelt ? pBelt : xBelt);
             }
         }
     }
-    
-    void ChangeSprite(Sprite sprite)
+
+    private void ChangeSprite(Sprite sprite)
     {
-        _spriteRenderer.sprite = sprite; 
+        spriteRenderer.sprite = sprite;
     }
 }
--- Assets/Scripts/Victory.cs
+++ Assets/Scripts/Victory.cs
@@ -1,31 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Victory : MonoBehaviour
 {
-    public GameObject victoryUI;
-    void Start()
+    [SerializeField] private GameObject victoryUI;
+
+    private void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        GameObject[] belts = GameObject.FindGameObjectsWithTag("belt");
-        var counter = 0;
-        foreach (var belt in belts) {
-            Swap c = belt.GetComponent<Swap>();
-
-            if (c._isCorrect) {
-                counter++;
-            }
-        }
-
+        var belts = GameObject.FindGameObjectsWithTag("belt");
+        var counter = belts.Select(belt => belt.GetComponent<Swap>()).Count(c => c.isCorrect);
         if (counter == belts.Length)
         {
             victoryUI.SetActive(true);
+            var balls = GameObject.Find("Balls").transform;
+            foreach (Transform ball in balls) ball.gameObject.GetComponent<RotateBall>().enabled = true;
         }
     }
 }
