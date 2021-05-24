788282e288ec760e609722110b46ff5cb4d0cb45
Roman <carapacik@gmail.com>
2021-05-24 18:45:01 +0300

    fixes for Moroz
    change textures: shaft, all beltz, BG3, BG8, BG9, LvlIcon, HintIcon, Arrow

--- Assets/Scripts/BackgroundChoose.cs
+++ Assets/Scripts/BackgroundChoose.cs
@@ -7,7 +7,7 @@ public class BackgroundChoose : MonoBehaviour
 
     private void Start()
     {
-        if (backgroundNumber == 0) backgroundNumber = Random.Range(1, 7);
+        if (backgroundNumber == 0) backgroundNumber = Random.Range(1, 9);
         var bg = Resources.Load<GameObject>($"Prefabs/Backgrounds/BG{backgroundNumber}").transform;
         Instantiate(bg, new Vector3(0, 0, 90), Quaternion.identity).SetParent(parentCanvas.transform);
     }
