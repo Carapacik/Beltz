a0e936e4f1a94ba73442d34cabb15e665bc65690
Roman <carapacik@gmail.com>
2021-05-28 21:55:44 +0300

    Add some features #1
    Add new victory animation
    Fix: victory sound played during SoundOff 
    Update for new unity version
    Update loading screen

---
+++ Assets/Scripts/AfterVictory.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AfterVictory : MonoBehaviour
+{
+    private void Awake()
+    {
+        if (PlayerPrefs.GetString("Sound") != "OFF")
+            GetComponent<AudioSource>().Play();
+    }
+
+    private void NextLevel()
+    {
+        var currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentSceneBuildIndex == 13)
+            LevelMenu();
+        else
+            SceneManager.LoadScene(currentSceneBuildIndex + 1);
+    }
+
+    private void LevelMenu()
+    {
+        SceneManager.LoadScene("LevelMenu");
+    }
+}
