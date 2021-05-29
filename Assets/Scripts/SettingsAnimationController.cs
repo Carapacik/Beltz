407e5c7c6886d66d6ebe33e2d5402689c75cf702
Roman <carapacik@gmail.com>
2021-05-29 17:56:23 +0300

    Pitch for main theme in settings active

--- Assets/Scripts/SettingsAnimationController.cs
+++ Assets/Scripts/SettingsAnimationController.cs
@@ -3,26 +3,25 @@ using UnityEngine;
 public class SettingsAnimationController : MonoBehaviour
 {
     private GameObject[] _belts;
+    private AudioSource _music;
+
 
     private void Start()
     {
+        _music = ObjectExtension.GetSavedObjects()[0].transform.GetComponent<AudioSource>();
         _belts = GameObject.FindGameObjectsWithTag("Belt");
     }
 
     private void OnClose()
     {
+        VictoryController.SwapAllBelts(_belts, true);
+        _music.pitch = 1f;
         gameObject.SetActive(false);
     }
 
-    private void BeltsDisabling()
+    private void OnOpen()
     {
-        foreach (var belt in _belts)
-            belt.gameObject.GetComponent<SwapBelts>().enabled = false;
-    }
-
-    private void BeltsEnabling()
-    {
-        foreach (var belt in _belts)
-            belt.gameObject.GetComponent<SwapBelts>().enabled = true;
+        VictoryController.SwapAllBelts(_belts, false);
+        _music.pitch = 0.85f;
     }
 }
