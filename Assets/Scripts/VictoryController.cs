36cd87abdbaa48989a7588e105286fac89a9a9df
Roman <carapacik@gmail.com>
2021-05-29 17:56:23 +0300

    Pitch for main theme in settings active

--- Assets/Scripts/VictoryController.cs
+++ Assets/Scripts/VictoryController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
@@ -5,26 +6,33 @@ public class VictoryController : MonoBehaviour
 {
     [SerializeField] private GameObject victoryObject;
     [SerializeField] private int currentLvl;
+    private GameObject[] _belts;
 
     private void Update()
     {
-        var belts = GameObject.FindGameObjectsWithTag("Belt");
-        var correctCounter = belts.Select(belt => belt.GetComponent<SwapBelts>())
+        _belts = GameObject.FindGameObjectsWithTag("Belt");
+        var correctCounter = _belts.Select(belt => belt.GetComponent<SwapBelts>())
             .Count(x => x.isCorrect);
-        if (correctCounter == belts.Length)
+        if (correctCounter == _belts.Length)
         {
             victoryObject.SetActive(true);
 
             var shafts = GameObject.Find("Shafts").transform;
             foreach (Transform shaft in shafts)
                 shaft.gameObject.GetComponent<RotateShaft>().enabled = true;
-            foreach (var belt in belts)
-                belt.gameObject.GetComponent<SwapBelts>().enabled = false;
+
+            SwapAllBelts(_belts, false);
 
             PlayerPrefs.SetInt("LastCompletedLevel", currentLvl);
             if (currentLvl > PlayerPrefs.GetInt("HighestLevel"))
                 PlayerPrefs.SetInt("HighestLevel", currentLvl);
             PlayerPrefs.Save();
         }
+    }
+
+    public static void SwapAllBelts(IEnumerable<GameObject> belts, bool swapEnabled)
+    {
+        foreach (var belt in belts)
+            belt.gameObject.GetComponent<SwapBelts>().enabled = swapEnabled;
     }
 }
