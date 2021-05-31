9ba4c6ec483b2a5567ef84f3d083735da341c557
Roman <carapacik@gmail.com>
2021-05-31 14:51:38 +0300

    Added tutorial 🏫

--- Assets/Scripts/VictoryAnimationController.cs
+++ Assets/Scripts/VictoryAnimationController.cs
@@ -10,9 +10,21 @@ public class VictoryAnimationController : MonoBehaviour
 
     private void NextLevel()
     {
-        var currentSceneBuildIndex = SceneManager.GetActiveScene().buildIndex;
-        if (currentSceneBuildIndex == 13) LevelMenu();
-        else SceneManager.LoadScene(currentSceneBuildIndex + 1);
+        var currentScene = SceneManager.GetActiveScene();
+        if (currentScene.name != "Lvl12")
+        {
+            SceneManager.LoadScene(currentScene.buildIndex + 1);
+        }
+        else if (PlayerPrefs.GetInt("Secret") > 1)
+        {
+            SceneManager.LoadScene("LvlSecret");
+        }
+        else
+        {
+            var secretCounter = PlayerPrefs.GetInt("Secret") + 1;
+            PlayerPrefs.SetInt("Secret", secretCounter);
+            LevelMenu();
+        }
     }
 
     private void LevelMenu()
