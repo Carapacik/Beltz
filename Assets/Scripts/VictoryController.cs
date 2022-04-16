09b312b38a8c1d48e706f919ca3b973deb080b35
Carapacik <carapacik@gmail.com>
2022-04-16 23:14:45 +0300

    Add victory controller

---
+++ Assets/Scripts/VictoryController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class VictoryController : MonoBehaviour
+{
+    [SerializeField] private GameObject victoryObject;
+    [SerializeField] private int currentLvl;
+    private GameObject[] _belts;
+
+    private void Start()
+    {
+        _belts = GameObject.FindGameObjectsWithTag("Belt");
+    }
+
+    private void Update()
+    {
+        var correctCounter = _belts.Select(belt => belt.GetComponent<SwapBeltz>()).Count(x => x.isCorrect);
+        if (correctCounter != _belts.Length) return;
+
+        victoryObject.SetActive(true);
+        var shafts = GameObject.Find("Shafts").transform;
+        foreach (Transform shaft in shafts) shaft.gameObject.GetComponent<RotateShaft>().enabled = true;
+        foreach (var belt in _belts) belt.gameObject.GetComponent<SwapBeltz>().enabled = false;
+        PlayerPrefs.SetInt("LastCompletedLevel", currentLvl);
+        if (currentLvl > PlayerPrefs.GetInt("HighestLevel")) PlayerPrefs.SetInt("HighestLevel", currentLvl);
+        PlayerPrefs.Save();
+    }
+}
