f4c8235ce901c9a431819d9da5c294242ea0d84a
Carapacik <carapacik@gmail.com>
2022-04-19 14:22:12 +0300

    Fixes for animation

--- Assets/Scripts/SettingsAnimationsController.cs
+++ Assets/Scripts/SettingsAnimationsController.cs
@@ -7,6 +7,7 @@ public class SettingsAnimationsController : MonoBehaviour
     [SerializeField] private GameObject settingsBox;
     [SerializeField] private GameObject shadowBackground;
     private GameObject[] _belts;
+    private bool _animationProgress = false;
 
     private void Start()
     {
@@ -15,6 +16,8 @@ public class SettingsAnimationsController : MonoBehaviour
 
     public void OpenSettings()
     {
+    if (!_animationProgress){
+            _animationProgress = true;
         shadowBackground.transform.GetComponent<Image>().DOFade(0.5f, 1);
         settingsBox.transform.DOLocalMoveY(760, 0);
         ObjectExtension.GetSavedObjects()[0].transform.GetComponent<AudioSource>().DOPitch(0.8f, 1);
@@ -23,19 +26,25 @@ public class SettingsAnimationsController : MonoBehaviour
             {
                 foreach (var belt in _belts) belt.gameObject.GetComponent<SwapBeltz>().enabled = false;
             });
+            }
     }
 
     public void CloseSettings()
     {
+        if (!_animationProgress){
+            _animationProgress = true;
         shadowBackground.transform.GetComponent<Image>().DOFade(0, 1);
-        settingsBox.transform.DOLocalMoveY(520, 0);
-        ObjectExtension.GetSavedObjects()[0].transform.GetComponent<AudioSource>().DOPitch(1, 1);
-        settingsBox.transform.DOLocalMoveY(760, 1)
+            settingsBox.transform.DOLocalMoveY(520, 0);
+            ObjectExtension.GetSavedObjects()[0].transform.GetComponent<AudioSource>().DOPitch(1, 1);
+            settingsBox.transform.DOLocalMoveY(760, 1)
             .OnComplete(() =>
             {
                 foreach (var belt in _belts) belt.gameObject.GetComponent<SwapBeltz>().enabled = true;
                 shadowBackground.SetActive(false);
                 gameObject.SetActive(false);
+                _animationProgress = false;
             });
+        }
+  
     }
 }
