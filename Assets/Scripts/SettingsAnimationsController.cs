81060f66214904d80dfd75347bbc6507a4e9f92d
Carapacik <carapacik@gmail.com>
2022-04-19 17:17:21 +0300

    Fix issues with settings animation

--- Assets/Scripts/SettingsAnimationsController.cs
+++ Assets/Scripts/SettingsAnimationsController.cs
@@ -6,8 +6,8 @@ public class SettingsAnimationsController : MonoBehaviour
 {
     [SerializeField] private GameObject settingsBox;
     [SerializeField] private GameObject shadowBackground;
+    private bool _animationProgress;
     private GameObject[] _belts;
-    private bool _animationProgress = false;
 
     private void Start()
     {
@@ -16,8 +16,8 @@ public class SettingsAnimationsController : MonoBehaviour
 
     public void OpenSettings()
     {
-    if (!_animationProgress){
-            _animationProgress = true;
+        if (_animationProgress) return;
+        _animationProgress = true;
         shadowBackground.transform.GetComponent<Image>().DOFade(0.5f, 1);
         settingsBox.transform.DOLocalMoveY(760, 0);
         ObjectExtension.GetSavedObjects()[0].transform.GetComponent<AudioSource>().DOPitch(0.8f, 1);
@@ -25,18 +25,18 @@ public class SettingsAnimationsController : MonoBehaviour
             .OnComplete(() =>
             {
                 foreach (var belt in _belts) belt.gameObject.GetComponent<SwapBeltz>().enabled = false;
+                _animationProgress = false;
             });
-            }
     }
 
     public void CloseSettings()
     {
-        if (!_animationProgress){
-            _animationProgress = true;
+        if (_animationProgress) return;
+        _animationProgress = true;
         shadowBackground.transform.GetComponent<Image>().DOFade(0, 1);
-            settingsBox.transform.DOLocalMoveY(520, 0);
-            ObjectExtension.GetSavedObjects()[0].transform.GetComponent<AudioSource>().DOPitch(1, 1);
-            settingsBox.transform.DOLocalMoveY(760, 1)
+        settingsBox.transform.DOLocalMoveY(520, 0);
+        ObjectExtension.GetSavedObjects()[0].transform.GetComponent<AudioSource>().DOPitch(1, 1);
+        settingsBox.transform.DOLocalMoveY(760, 1)
             .OnComplete(() =>
             {
                 foreach (var belt in _belts) belt.gameObject.GetComponent<SwapBeltz>().enabled = true;
@@ -44,7 +44,5 @@ public class SettingsAnimationsController : MonoBehaviour
                 gameObject.SetActive(false);
                 _animationProgress = false;
             });
-        }
-  
     }
 }
--- Assets/Scripts/VictoryAnimationsController.cs
+++ Assets/Scripts/VictoryAnimationsController.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 public class VictoryAnimationsController : MonoBehaviour
 {
     [SerializeField] private GameObject victoryBox;
+    [SerializeField] private GameObject[] needToHide;
 
     private void Start()
     {
         PlayVictorySound();
         gameObject.transform.GetComponent<Image>().DOFade(0.6f, 2).OnComplete(() =>
         {
+            foreach (var item in needToHide) item.SetActive(false);
             victoryBox.SetActive(true);
             victoryBox.transform.DOScale(1, 1);
         });
